d417116b840a4eb1726491956da86e4431886a24
Caleb <calebkugel1@gmail.com>
2021-07-14 19:50:52 -0500

    w/subs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "LinkedList.h"
 
 using namespace std;
+typedef unsigned char uint8;
 
 void strReplacement(string &str, const char& toBeReplaced, char toReplaceWith) {
     while (str.find(toBeReplaced) >= 0 && str.find(toBeReplaced) <= str.size()) {
@@ -18,9 +19,8 @@ void strReplacement(string &str, const char& toBeReplaced, char toReplaceWith) {
     }
 }
 
-void readFile(string path) {
+LinkedList readFile(string path) {
     cout << path << endl;
-/*    typedef unsigned char uint8;
     ifstream source("path", ios_base::binary);
     LinkedList list;
     while (source) {
@@ -30,14 +30,45 @@ void readFile(string path) {
         ios::pos_type after = source.tellg();
         Node *temp = new Node(reinterpret_cast<void *>(x));
         list.push(temp);
-    }*/
+    }
+    return list;
 }
 
-vector<filesystem::path *> getFileNames() {
-    string path = "E:\\coding";
+LinkedList checkForDuplicates(vector<filesystem::path *> pathers) {
+    LinkedList toBeRemoved;
+    vector<filesystem::path> pathes = (vector<filesystem::path>&) pathers;
+    for (int i = 0; i < pathes.size(); i++) {
+        for (int k = 0; k < pathes.size(); k++) {
+            if (file_size(pathes[i]) != file_size(pathes[k]) ||
+                pathes[i].filename().generic_string()._Equal(pathes[k].filename().generic_string())) {
+                cout << pathes[i].generic_string() + " in terms of size and extension is different from: " +
+                        pathes[k].generic_string() << endl;
+            } else {
+                bool NotDif = false;
+                LinkedList one = readFile(pathes[i].generic_string());
+                LinkedList two = readFile(pathes[k].generic_string());
+                while (one.peak() != nullptr || two.peak() != nullptr) {
+                    if ((uint8) one.pop().getData() == (uint8) two.pop().getData()) {
+
+                    }
+                    else {
+                        NotDif = true;
+                    }
+                }
+                if (NotDif) {
+                    toBeRemoved.push(new Node((void*) &pathes[i]));
+                }
+            }
+        }
+    }
+    return toBeRemoved;
+}
+
+vector<filesystem::path> getFileNames() {
+    string path = R"(E:\coding\duplicate_checker\test)";
     string temp;
     vector<string> pathsTemp;
-    vector<filesystem::path*> pathsFiles;
+    vector<filesystem::path> pathsFiles;
     for (const auto & entry : filesystem::directory_iterator(path)) {
         temp = entry.path().generic_string();
         pathsTemp.push_back(temp);
@@ -53,7 +84,7 @@ vector<filesystem::path *> getFileNames() {
             }
         }
         else if (is_regular_file(path1)) {
-            pathsFiles.emplace_back(path1);
+            pathsFiles.push_back(path1);
         }
         else {
             cout << "scuffed path: " + pathsTemp[i];
@@ -68,6 +99,13 @@ vector<filesystem::path *> getFileNames() {
 
 int main() {
     getFileNames();
+/*    LinkedList temp = checkForDuplicates(getFileNames());
+
+    while (temp.peak() != nullptr) {
+        filesystem::path *a = (filesystem::path*) (temp.pop().getData());
+        cout << a->generic_string().c_str() << endl;
+        remove(a->generic_string().c_str());
+    }*/
 
 
     return 0;
